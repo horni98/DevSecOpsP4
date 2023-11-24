bb7b03a120be8d6fd9bf898d723253345fb8610f
Felix Horn <horn.felix98@gmail.com>
2023-11-24 11:59:24 +0100

    Fixed query

--- PHPAppSnippets/low.php
+++ PHPAppSnippets/low.php
@@ -6,11 +6,17 @@ if( isset( $_GET[ 'Login' ] ) ) {
 
 	// Get password
 	$pass = $_GET[ 'password' ];
-	$pass = hash( 'sha256',$pass );
+	$pass = hash('sha256', $pass );
 
 	// Check the database
-	$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
-	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+	$query  = "SELECT * FROM users WHERE user = :user AND password = :pass;";
+	$stmt = $this->conn->prepare($query);
+	$stmt->bind_param(":user", $user);
+	$stmt->bind_param(":pass", $pass);
+	$stmt->execute();
+	//$result = mysqli_query($GLOBALS["_mysqli_ston"],  $query ) or die( '<pre>' . ((isobject($GLOBALS["mysqli_ston"])) ? mysqli_error($GLOBALS["_mysqliston"]) : (($mysqli_res = mysqli_connect_error()) ? $_mysqli_res : false)) . '</pre>' );
+
+	$result = $stmt->store_result();
 
 	if( $result && mysqli_num_rows( $result ) == 1 ) {
 		// Get users details
@@ -19,14 +25,14 @@ if( isset( $_GET[ 'Login' ] ) ) {
 
 		// Login successful
 		$html .= "<p>Welcome to the password protected area {$user}</p>";
-		$html .= "<img src=\"{$avatar}\" />";
-	}
+		$html .= "<img src="{$avatar}" />";
+    }
 	else {
 		// Login failed
 		$html .= "<pre><br />Username and/or password incorrect.</pre>";
 	}
 
-	((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
+	((isnull($mysqli_res = mysqli_close($GLOBALS["_mysqliston"]))) ? false : $mysqli_res);
 }
 
 ?>
